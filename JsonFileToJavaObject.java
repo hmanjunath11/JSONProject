8883d0ca366acabf0f2c8a4f6ce001bdf648ee5c
hmanjunath11 <52601061+hmanjunath11@users.noreply.github.com>
2020-01-02 23:47:28 +0530

    Add files via upload

---
+++ JsonFileToJavaObject.java
@@ -0,0 +1,44 @@
+package com.json.examples.jsonExamples;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import com.json.javaObjects.Staff;
+
+public class JsonFileToJavaObject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        
+
+            // Convert JSON File to Java Object
+            
+			try {
+			Staff staff = gson.fromJson(new FileReader("/Users/manjunathah/FT-Automation-workspace_Karate/jsonExamples/src/jsonSource/staff.json"), Staff.class);
+			// print staff 
+            System.out.println(staff);
+			} catch (JsonSyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonIOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+
+
+    
+
+	}
+
+}
